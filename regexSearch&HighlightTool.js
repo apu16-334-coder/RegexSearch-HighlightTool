b02d6f7baf86cce6ecda1ee9567a1608b24581e1
Muhammad Apu <apu16-334@diu.edu.bd>
2025-12-10 23:44:46 +0600

    add regex flags parts

--- regexSearch&HighlightTool.js
+++ regexSearch&HighlightTool.js
@@ -7,21 +7,45 @@
     const result = document.getElementById("result")
 
     highlightBtn.addEventListener("click", () => {
-        if (regex.value!=="" && text.value!=="") {
+        if (regex.value !== "" && text.value !== "") {
             showingResultBox();
         }
     })
 
-    function showingResultBox(){
+
+
+    function showingResultBox() {
         let pattern = regex.value
+        console.log(pattern)
         const regexObj = new RegExp(pattern);
+        console.log(text.value.match(regexObj))
 
         const highlighted = text.value.replace(regexObj, match => `<span class="highlight">${match}</span>`);
 
-        console.log(text.value.replace(regex, match => `<span class="highlight">${match}</span>`))
-
-        result.innerHTML= highlighted || "No Match Found"
+        result.innerHTML = highlighted || "No Match Found"
 
     }
+
+    // 
+    const flagBtn=document.getElementById("flags")
+    const dropdownContent = document.querySelector(".dropdown-content")
+    dropdownContent.addEventListener("click", markFlags)
+
+    function markFlags(e){
+
+        const flag=e.target
+        if(flag.classList.contains("mark")){           
+            flag.textContent=flag.textContent.replace(" ✓","");
+            flag.classList.remove("mark")
+        }else{
+            flag.textContent+=" ✓";
+            flag.classList.add("mark")
+        }
+        
+        
+    }
+    
+
+
 })()
 
