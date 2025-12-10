bad1f418d644741dfbfb3a99ba6263c64f3fb824
Muhammad Apu <apu16-334@diu.edu.bd>
2025-12-11 00:54:01 +0600

    added regex flag JavaScript part

--- regexSearch&HighlightTool.js
+++ regexSearch&HighlightTool.js
@@ -5,6 +5,7 @@
     const highlightBtn = document.getElementById("highlight")
     const clearBtn = document.getElementById("clear")
     const result = document.getElementById("result")
+    const flagBtn=document.getElementById("flags")
 
     highlightBtn.addEventListener("click", () => {
         if (regex.value !== "" && text.value !== "") {
@@ -15,9 +16,9 @@
 
 
     function showingResultBox() {
-        let pattern = regex.value
-        console.log(pattern)
-        const regexObj = new RegExp(pattern);
+        let pattern = regex.value;
+        let flag=(flagBtn.value==="flags")? "": flagBtn.value;
+        const regexObj = new RegExp(pattern,flag);
         console.log(text.value.match(regexObj))
 
         const highlighted = text.value.replace(regexObj, match => `<span class="highlight">${match}</span>`);
@@ -26,26 +27,35 @@
 
     }
 
-    // 
-    const flagBtn=document.getElementById("flags")
+    
     const dropdownContent = document.querySelector(".dropdown-content")
     dropdownContent.addEventListener("click", markFlags)
-
+    
     function markFlags(e){
-
         const flag=e.target
         if(flag.classList.contains("mark")){           
-            flag.textContent=flag.textContent.replace(" ✓","");
+            flag.innerHTML=flag.innerHTML.replace(" ✓","");
             flag.classList.remove("mark")
         }else{
-            flag.textContent+=" ✓";
+            flag.innerHTML+=" ✓";
             flag.classList.add("mark")
         }
-        
-        
-    }
-    
 
+        const divs = document.querySelectorAll(".dropdown-content div")
+        let result=""
+        for (const div of divs) {
+            if(div.classList.contains("mark")){
+                result+=div.firstElementChild.textContent;
+            }
+        }
+        if(result===""){
+            flagBtn.value="flags"
+        }else{
+            flagBtn.value=result;
+        }
+
+    }
+        
 
 })()
 
