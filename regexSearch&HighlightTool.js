0762f28de451e3f75d442182262d02f29ec6c380
Muhammad Apu <apu16-334@diu.edu.bd>
2025-12-13 21:53:34 +0600

    adding match information if user do not choose g flag

--- regexSearch&HighlightTool.js
+++ regexSearch&HighlightTool.js
@@ -7,24 +7,57 @@
     const result = document.getElementById("result")
     const flagBtn=document.getElementById("flags")
 
+    text.value="thiss iss it"
+
     highlightBtn.addEventListener("click", () => {
         if (regex.value !== "" && text.value !== "") {
             showingResultBox();
         }
     })
 
-
-
     function showingResultBox() {
         let pattern = regex.value;
-        let flag=(flagBtn.value==="flags")? "": flagBtn.value;
+        let flag=(flagBtn.value==="flags")? "d": flagBtn.value+"d";
+        console.log(flag)
         const regexObj = new RegExp(pattern,flag);
-        console.log(text.value.match(regexObj))
+        
+        // try {
+            const highlighted = text.value.replace(regexObj, match => `<span class="highlight">${match}</span>`);
 
-        const highlighted = text.value.replace(regexObj, match => `<span class="highlight">${match}</span>`);
+            result.innerHTML = highlighted || "No Match Found"
 
-        result.innerHTML = highlighted || "No Match Found"
+            let myArray;
+            if(flag.includes("g")){
+                console.log("hello")
+            }else{
+                myArray =regexObj.exec(text.value)
+                console.log(myArray)
+                const matchSection=document.createElement("h4");
+                matchSection.style.color="rgba(2, 188, 2, 1)";
+                matchSection.textContent= `Match 1  (${myArray.indices[0][0]} – ${myArray.indices[0][1]})  |  '${myArray[0]}'`
+                result.appendChild(matchSection);
 
+                console.log(myArray.length)
+
+                for(let i=1; myArray.length>1 && i<myArray.length; i++){
+                    console.log("hello")
+                    const groupSection = document.createElement("span")
+                    groupSection.style.color="rgba(51, 29, 250, 1)"
+                    groupSection.textContent = `    Group ${i}  ${myArray.indices[i][0]} – ${myArray.indices[i][1]}  |  '${myArray[i]}'`;
+
+                    matchSection.appendChild(groupSection)
+                }
+                
+
+
+            }
+            
+        
+
+        // } catch (error) {
+        //     result.innerHTML = "Invalid Regex Pattern"
+        //     result.style.color="red";
+        // }
     }
 
     
@@ -58,4 +91,3 @@
         
 
 })()
-
